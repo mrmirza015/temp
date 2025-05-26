e908e38bdfec33c7d0894c127f638463e262fe30
Mirza Shahrukh Beg <92719827+mrmirza015@users.noreply.github.com>
2025-05-26 17:20:11 +0530

    Update stake.py

--- stake.py
+++ stake.py
@@ -42,29 +42,15 @@ ir = '1'
 isMobile = True
 isDesktop = False
 sr_mobile_arr = [
-    '360×800', '414×896', '360×640', '412×915', '390×844',
-    '360×780', '375×667', '375×812', '360×760', '393×851',
-    '393×873', '412×892', '428×926', '360×720', '385×854',
-    '412×869', '414×736', '412×846', '360×740', '384×854'
+    '360×800'
 ]
 uam_arr = [
-    'SM-G955U', 'SM-G981B', 'SM-F946U', 'SM-A715F', 'SM-G780F',
-    'SM-G985U', 'SM-N975F', 'SM-A515F', 'SM-G770F', 'SM-M315F',
-    'Pixel 6', 'Pixel 6 Pro', 'Pixel 5', 'Pixel 4a', 'Pixel 4',
-    'Pixel 3 XL', 'OnePlus 9', 'OnePlus 9 Pro', 'OnePlus 8T',
-    'OnePlus 8', 'OnePlus Nord', 'OnePlus 7T', 'Redmi Note 11',
-    'Mi 11', 'Mi 11 Ultra', 'Redmi Note 10 Pro', 'Poco X3 Pro',
-    'Redmi K40', 'Find X3 Pro', 'Reno 6 Pro', 'A94 5G', 'Reno 5 Pro',
-    'Find X2', 'Reno 4 Pro', 'Moto G Power 2021', 'Moto G Stylus 2021',
-    'Edge 20', 'Edge 20 Pro', 'Moto G100', 'Moto E 2020', 'Xperia 1 III',
-    'Xperia 5 III', 'Xperia 10 III', 'Xperia 1 II', 'Xperia 5 II', 'Xperia 10 II', 'iPhone 15 Plus', 'iPhone 15 Pro Max', 'iPhone 14 Plus', 'iPhone 14 Pro', 'iPhone 14 Pro Max', 'iPhone 13', 'iPhone 13 Pro', 'iPhone 13 Pro Max', 'iPhone 12', 'iPhone 12 Mini', 'iPhone 12 Pro', 'iPhone 11', 'iPhone 11 Pro', 'iPhone 11 Pro Max', 'iPhone SE (3rd Gen)', 'iPhone SE (2nd Gen)', 'iPhone XR', 'iPhone XS', 'iPhone XS Max']
+    'SM-G955U',]
 
 
 # Desktop Configurations
 uaa_desktop_arr = [
-    "x86", "x86_64", "arm", "arm64", "mips", "mips64", "ppc",
-    "ppc64", "sparc", "sparc64", "ia64", "riscv", "riscv64",
-    "s390", "s390x"
+    "x86",
 ]
 
 
@@ -77,10 +63,7 @@ userAgentMapping = {
 
 
 sr_desktop_arr = [
-    '1366x768', '1920x1080', '2560x1440', '3840x2160', '1280x800',
-    '1600x900', '2560x1600', '2880x1800', '3200x1800', '3840x2400',
-    '1920x1080', '2560x1440', '3840x2160', '5120x1440', '3840x1600',
-    '7680x4320', '1600x1200', '2048x1152', '3440x1440', '2560x1080'
+    '1366x768'
 ]
 
 
@@ -124,12 +107,10 @@ def generate_urls():
         uap = urllib.parse.quote(random_key)
         uafvl = urllib.parse.quote(random_value)
 
-    firstCall = f"{baseUrl}v={version}&tid={tid}&gtm={gtm}&_p={p}&gcd={gcd}&npa={npa}&dma={dma}&tag_exp={tag_exp}&cid={cid}&ul={ul}&sr={sr}&ir={ir}&uaa={uaa}&uab={uab}&uafvl={uafvl}&uamb={uamb}&uam={uam}&uap={uap}&uapv=15.0.0&uaw=0&are=1&pae=1&frm=0&pscdl=noapi&_eu=Ag&_s=1&sid={sid}&sct=1&seg=0&dl={uri_dl}&dt={uri_dt}&en=page_view&_fv=1&_ss=1&_ee=1&tfd={tfd1}&_z=fetch&ep.origin=firebase"
-
     tracker = f"{baseUrl}v={version}&tid=G-WZXPX61NKF&gtm={gtm}&_p={p}&gcd={gcd}&npa={npa}&dma={dma}&tag_exp={tag_exp}&cid={cid}&ul={ul}&sr={sr}&ir={ir}&uaa={uaa}&uab={uab}&uafvl={uafvl}&uamb={uamb}&uam={uam}&uap={uap}&uapv=15.0.0&uaw=0&are=1&pae=1&frm=0&pscdl=noapi&_eu=EEA&_s=2&sid={sid}&sct=1&seg=1&dl={uri_dl}&dt={uri_dt}&en=Recrd&_ee=1&tfd={tfd1}&_z=fetch"
 
     return {
-        'firstCall': firstCall,
+
 
         'tracker': tracker
 
@@ -200,7 +181,9 @@ for device in devices:
             f"--window-size={device['width']},{device['height']}")
 
     print(f"Opening as {device['name']}")
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(
+        executable_path="/usr/local/bin/chromedriver", options=options)
+
     try:
         driver.get(url)
         urls = generate_urls()
